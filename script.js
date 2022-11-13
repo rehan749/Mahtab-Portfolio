f1c53074c7692b1a6c87f810662a9ddc41df83a2
MAHTAB ALAM <100292587+rehan749@users.noreply.github.com>
2022-11-13 20:42:19 +0530

    new comminy

--- script.js
+++ script.js
@@ -41,12 +41,12 @@ $(document).ready(function(){
         loop: true
     });
 
-    var typed = new Typed(".typing-2", {
-        strings: ["Developer", "Designer", "UI-UX Designer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
+    // var typed = new Typed(".typing-2", {
+    //     strings: ["Developer", "Designer", "UI-UX Designer"],
+    //     typeSpeed: 100,
+    //     backSpeed: 60,
+    //     loop: true
+    // });
 
     // owl carousel script
     $('.carousel').owlCarousel({
