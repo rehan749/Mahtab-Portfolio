89efb5f0f4e09ff63c16d57eeae0b4ab405ac9e5
MAHTAB ALAM <100292587+rehan749@users.noreply.github.com>
2024-01-30 17:24:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: [ "Developer", "Designer", "UI-UX Designer"],
+        strings: [ "Developer", "Designer", "UI-UX Designer","Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
